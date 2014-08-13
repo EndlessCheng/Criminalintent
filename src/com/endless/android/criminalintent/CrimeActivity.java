fb79b4d0a527e64d156316d1d7c46314e43723de
EndlessCheng <loli.con@qq.com>
2014-08-13 23:09:20 +0800

    初始化

---
+++ src/com/endless/android/criminalintent/CrimeActivity.java
@@ -0,0 +1,28 @@
+package com.endless.android.criminalintent;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+
+/**
+ * ÍÐ¹Ü UI fragment
+ */
+public class CrimeActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_crime);
+
+		FragmentManager fm = getFragmentManager();
+		Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+		if (fragment == null) {
+			fragment = new CrimeFragment();
+			fm.beginTransaction().add(R.id.fragmentContainer, fragment)
+					.commit();
+		}
+	}
+
+}
