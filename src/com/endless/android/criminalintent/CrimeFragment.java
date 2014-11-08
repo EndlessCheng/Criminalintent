61f88b886cdffb7b5b187e501accee4c0fd7aed9
EndlessCheng <loli.con@qq.com>
2014-11-08 16:36:10 +0800

    添加组件

--- src/com/endless/android/criminalintent/CrimeFragment.java
+++ src/com/endless/android/criminalintent/CrimeFragment.java
@@ -7,11 +7,17 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
 	private Crime mCrime;
 	private EditText mTitleField;
+	private Button mDateButton;
+	private CheckBox mSolvedCheckBox;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -24,7 +30,7 @@ public class CrimeFragment extends Fragment {
 			Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_crime, parent, false);
 		// false：在 activity 中手动添加视图，而不是自动添加给父视图
-		
+
 		mTitleField = (EditText) v.findViewById(R.id.crime_title);
 		mTitleField.addTextChangedListener(new TextWatcher() {
 
@@ -47,7 +53,23 @@ public class CrimeFragment extends Fragment {
 
 			}
 		});
-		
+
+		mDateButton = (Button) v.findViewById(R.id.crime_date);
+		mDateButton.setText(mCrime.getDate().toString());
+		mDateButton.setEnabled(false);
+
+		mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+		mSolvedCheckBox
+				.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+					@Override
+					public void onCheckedChanged(CompoundButton buttonView,
+							boolean isChecked) {
+						mCrime.setSolved(isChecked);
+					}
+
+				});
+
 		return v;
 	}
 }
